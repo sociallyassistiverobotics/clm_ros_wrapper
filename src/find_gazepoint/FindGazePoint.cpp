99063bd14da6c30980ff61011239073e9001e94c
yunustuncbilek <yunus.tuncbilek@yale.edu>
2016-06-28 15:54:23 -0400

    Moved find_gazepoint to subfolder. Added feature to read screen dimensions from launch file.

--- src/find_gazepoint/FindGazePoint.cpp
+++ src/find_gazepoint/FindGazePoint.cpp
@@ -5,7 +5,6 @@ point of the head direction and the hardcoded screen. It then publishes this geo
 publisher topic "/clm_ros_wrapper/gaze_point".
 Yunus
 */
-
 #include "CLM_core.h"
 
 #include <fstream>
@@ -37,28 +36,27 @@ Yunus
 
 #include <geometry_msgs/Vector3.h> 
 
+int screenAngleInDegrees; 
+float screenWidth;
+float screenHeight;
+float screenAngle;
+
 using namespace std;
 using namespace cv;
 
 using namespace boost::filesystem;
 
-// to publish the gaze point
 ros::Publisher gazepoint_pub;
 
 tf::Vector3 headposition_cf;
 
 void vector_callback(const geometry_msgs::Vector3::ConstPtr& msg)
 {
-  // head fixation vector
-  tf::Vector3 hfv_cf;
+   tf::Vector3 hfv_cf;
 
-  // the hardcoded properties of the screen
-  float screenAngle = M_PI_4; // representing 45 degrees
-  float screenWidth = 520;
-  float screenHeight = 320;
+   tf::vector3MsgToTF(*msg, hfv_cf);
 
-  // generating the hfv_cf from the message, msg
-	tf::vector3MsgToTF(*msg, hfv_cf);
+   //cout << screenAngle << "   " << screenWidth << "   " << screenHeight;
 
 	// rotation matrix from camera frame to world frame
    tf::Matrix3x3 matrix_cf2wf;
@@ -67,7 +65,7 @@ void vector_callback(const geometry_msgs::Vector3::ConstPtr& msg)
    // translation vector from cf to wf
    tf::Vector3 vector_cf2wf = tf::Vector3(0, sin(screenAngle) * screenHeight, cos(screenAngle) * screenHeight);
 
-   // transformation from th camera frame to the world frame
+   // transformation from the camera frame to the world frame
    tf::Transform transfrom_cf2wf = tf::Transform(matrix_cf2wf, vector_cf2wf);
 
    //storing the locations of the lower corners of screen and the camera 
@@ -75,24 +73,18 @@ void vector_callback(const geometry_msgs::Vector3::ConstPtr& msg)
    tf::Vector3 lower_left_corner_of_screen_wf = tf::Vector3(screenWidth / 2, 0, 0);
    tf::Vector3 lower_right_corner_of_screen_wf = tf::Vector3( -1 * screenWidth / 2, 0, 0);
    // the location of the camera in the world frame would be equal to the translation vector
-   tf::Vector3 camera_wf = vector_cf2wf;
+   tf::Vector3 camera_wf = tf::Vector3(0, cos(screenAngle) * screenHeight, sin(screenAngle) * screenHeight);
 
-   //rotating the head fixation vector
    tf::Vector3 hfv_wf = matrix_cf2wf.inverse() * (hfv_cf);
 
    // storing the head position in the camera frame
    tf::Vector3 headposition_wf = transfrom_cf2wf(headposition_cf);
 
-   //below are two tests of the head position and hfv in the worldframe:
-
-   // below test was successful with 45 deg, 350 height and 520 width
+   //successful with 45 deg, 350 height and 520 width
    //cout <<  "HEAD POSITION x:" << headposition_wf.getX() << " \t y:" << headposition_wf.getY() << "\t z:" << headposition_wf.getZ() << "\n";
    
    // successful
    //cout <<  "HFV x:" << hfv_wf.getX() << " \t y:" << hfv_wf.getY() << "\t z:" << hfv_wf.getZ() << "\n";
-
-   //getting the coordinates of a point on the gaze direction to construct the line
-   //(I all it random because the constant 100 carries no meaning)
    tf::Vector3 randompoint_on_gazedirection_wf = headposition_wf + 100 * hfv_wf;
 
    // using the Line-Plane intersection formula on Wolfram link: http://mathworld.wolfram.com/Line-PlaneIntersection.html
@@ -110,7 +102,7 @@ void vector_callback(const geometry_msgs::Vector3::ConstPtr& msg)
        camera_wf.getY(), lower_right_corner_of_screen_wf.getY(), lower_left_corner_of_screen_wf.getY(), randompoint_on_gazedirection_wf.getY() - headposition_wf.getY(),
        camera_wf.getZ(), lower_right_corner_of_screen_wf.getZ(), lower_left_corner_of_screen_wf.getZ(), randompoint_on_gazedirection_wf.getZ() - headposition_wf.getZ());
 
-   //following the formula, I calculate t -- check the link
+   // following the formula, I calculate t -- check the link
    double determinant_ratio = (-1) * cv::determinant(matrix1) / cv::determinant(matrix2);
 
    // finally I plug in the determinant ratio (t) to get the intersection point
@@ -122,7 +114,7 @@ void vector_callback(const geometry_msgs::Vector3::ConstPtr& msg)
    gazepoint_pub.publish(gazepoint_msg);
 }
 
-void headposition_callback(const geometry_msgs::Vector3::ConstPtr& msg)
+void headposition_callback(const clm_ros_wrapper::ConstPtr& msg)
 {
 	tf::vector3MsgToTF(*msg, headposition_cf);
 }
@@ -134,15 +126,17 @@ int main(int argc, char **argv)
 	ros::Subscriber vector_sub;
 	ros::NodeHandle nh;
 
-	//tf::Vector3 headposition_cf;
+	tf::Vector3 headposition_cf;
 
-  // topic to publish the gaze point
+	nh.getParam("find_gazepoint/screenAngleInDegrees", screenAngleInDegrees);
+   nh.getParam("find_gazepoint/screenWidth", screenWidth);
+   nh.getParam("find_gazepoint/screenHeight", screenHeight);
+
+   screenAngle = screenAngleInDegrees * M_PI_2 / 90;
+
 	gazepoint_pub = nh.advertise<geometry_msgs::Vector3>("clm_ros_wrapper/gaze_point", 1);
 
-  //subscriber for the head position in camera frame
 	headposition_sub = nh.subscribe("/clm_ros_wrapper/head_position", 1, &headposition_callback);
-
-  // subscriber for the head fixation vector in camera frame
 	vector_sub = nh.subscribe("/clm_ros_wrapper/head_vector", 1, &vector_callback);
 
 	ros::spin();

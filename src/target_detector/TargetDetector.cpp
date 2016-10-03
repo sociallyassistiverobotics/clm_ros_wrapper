a8e49a20b3486cc6df9bef01ad9a75d5cf128bca
Chien-Ming Huang <chienming.h@gmail.com>
2016-10-03 17:45:43 -0400

    template for the 12 points on screen

--- src/target_detector/TargetDetector.cpp
+++ src/target_detector/TargetDetector.cpp
@@ -351,22 +351,21 @@ void gazepoint_callback2(const clm_ros_wrapper::GazePointAndDirection::ConstPtr&
 
         if(found_a_match){
             //cout << "shortest distance = " << shortest_dist << endl;
-            if((shortest_target == 0) || (shortest_target == 1) || (shortest_target == 2)
-                || (shortest_target == 3) || (shortest_target == 4)){
-                std::cout << "..screen" << std::endl;
+            if((shortest_target >= 0) && (shortest_target <= 11){
+                // std::cout << "..screen" << std::endl;
                 estimated_region = detected_target.SCREEN;
             }
-            else if((shortest_target == 5) || (shortest_target == 6)){
-                std::cout << "..robot" << std::endl;
+            else if((shortest_target == 12) || (shortest_target == 13)){
+                // std::cout << "..robot" << std::endl;
                 estimated_region = detected_target.ROBOT;
             }
-            else if((shortest_target == 7) || (shortest_target == 8)){
-                std::cout << "..parent" << std::endl;
+            else if((shortest_target == 14) || (shortest_target == 15)){
+                // std::cout << "..parent" << std::endl;
                 estimated_region = detected_target.PARENT;
             }
         }
         else{
-            std::cout << "..others" << std::endl;
+            // std::cout << "..others" << std::endl;
             estimated_region = detected_target.OUTSIDE;
         }
 
@@ -437,24 +436,49 @@ int main(int argc, char **argv)
     ros::Subscriber gazepoint_sub = nh.subscribe("/sar/perception/gaze_point_and_direction_wf", 1, &gazepoint_callback2);
 
     // initialize virtual targets
-    tf::Vector3 virtual_screen_center = tf::Vector3(270,250,240);
-    tf::Vector3 virtual_screen_top_left = tf::Vector3(50,140,340);
+    // tf::Vector3 virtual_screen_center = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r1_c1 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r2_c1 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r3_c1 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r1_c2 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r2_c2 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r3_c2 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r1_c3 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r2_c3 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r3_c3 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r1_c4 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r2_c4 = tf::Vector3(270,250,240);
+    tf::Vector3 virtual_screen_r3_c4 = tf::Vector3(270,250,240);
+
+    //tf::Vector3 virtual_screen_top_left = tf::Vector3(50,200,340);
     // tf::Vector3 virtual_screen_top_right = tf::Vector3(400,380,260);
-    tf::Vector3 virtual_screen_top_right = tf::Vector3(370,350,340);
+    //tf::Vector3 virtual_screen_top_right = tf::Vector3(370,350,340);
     // tf::Vector3 virtual_screen_bottom_left = tf::Vector3(100,100,130);
-    tf::Vector3 virtual_screen_bottom_left = tf::Vector3(50,140,150);
+    //tf::Vector3 virtual_screen_bottom_left = tf::Vector3(50,140,130);
     // tf::Vector3 virtual_screen_bottom_right = tf::Vector3(450,280,100);
-    tf::Vector3 virtual_screen_bottom_right s= tf::Vector3(420,310,150);
+    //tf::Vector3 virtual_screen_bottom_right = tf::Vector3(420,310,150);
     tf::Vector3 virtual_robot_top = tf::Vector3(640,170,300);
     tf::Vector3 virtual_robot_bottom = tf::Vector3(640,170,100);
     tf::Vector3 virtual_parent_1 = tf::Vector3(750,-100,300);
     tf::Vector3 virtual_parent_2 = tf::Vector3(750, 0,300);
 
-    virtual_targets.push_back(virtual_screen_center);
-    virtual_targets.push_back(virtual_screen_top_left);
-    virtual_targets.push_back(virtual_screen_top_right);
-    virtual_targets.push_back(virtual_screen_bottom_left);
-    virtual_targets.push_back(virtual_screen_bottom_right);
+    // virtual_targets.push_back(virtual_screen_center);
+    // virtual_targets.push_back(virtual_screen_top_left);
+    // virtual_targets.push_back(virtual_screen_top_right);
+    // virtual_targets.push_back(virtual_screen_bottom_left);
+    // virtual_targets.push_back(virtual_screen_bottom_right);
+    virtual_targets.push_back(virtual_screen_r1_c1);
+    virtual_targets.push_back(virtual_screen_r2_c1);
+    virtual_targets.push_back(virtual_screen_r3_c1);
+    virtual_targets.push_back(virtual_screen_r1_c2);
+    virtual_targets.push_back(virtual_screen_r2_c2);
+    virtual_targets.push_back(virtual_screen_r3_c2);
+    virtual_targets.push_back(virtual_screen_r1_c3);
+    virtual_targets.push_back(virtual_screen_r2_c3);
+    virtual_targets.push_back(virtual_screen_r3_c3);
+    virtual_targets.push_back(virtual_screen_r1_c4);
+    virtual_targets.push_back(virtual_screen_r2_c4);
+    virtual_targets.push_back(virtual_screen_r3_c4);
     virtual_targets.push_back(virtual_robot_top);
     virtual_targets.push_back(virtual_robot_bottom);
     virtual_targets.push_back(virtual_parent_1);

1989730627752006fd11af844b2779cd50be91df
Chien-Ming Huang <chienming.h@gmail.com>
2017-09-28 13:45:56 -0400

    revert change

--- run_sar_perception.sh
+++ run_sar_perception.sh
@@ -43,6 +43,5 @@ roslaunch clm_ros_wrapper clm_ros_wrapper.launch
 # roslaunch clm_ros_wrapper clm_ros_wrapper_record.launch
 
 # https://askubuntu.com/questions/187071/how-do-i-shut-down-or-reboot-from-a-terminal/187080
-python /home/sar/catkin_ws/src/sar_core/nodes/online.py down
 sleep 5s
 /usr/bin/dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
